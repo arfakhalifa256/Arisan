1fcdf0d839e8642b3417492851d5fec06c3959e2
arfakhalifa256 <arfakhalifa256@gmail.com>
2026-09-18 08:34:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,28 @@
-const CACHE_NAME = "arfa-v5";
-const URLS = ["./", "./index.html", "./arisan.html", "./manifest.json"];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
+const CACHE = "arfa-v6-final";
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./img/icon-192.png",
+        "./img/icon-512.png"
+      ]);
+    }).catch(err => console.log("Cache gagal, tapi biarin:", err))
+  );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
